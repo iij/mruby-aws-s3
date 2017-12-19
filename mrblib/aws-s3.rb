f4b25e24f4f5348096f4f1aec4aabd1dde15e828
Naughie <zomacchi@gmail.com>
2017-12-19 22:02:58 +0900

    Make compatible with prev ver.

--- mrblib/aws-s3.rb
+++ mrblib/aws-s3.rb
@@ -2,26 +2,30 @@ module AWS
   class S3
     EMPTY_STRING_SHA256 = Digest::SHA256::hexdigest('')
 
-    def initialize(access_key: nil, secret_key: nil, security_token: nil, region: nil, bucket_name: nil)
+    def initialize(access_key, secret_key, security_token = nil, region = nil)
       @access_key = access_key || ENV['AWS_ACCESS_KEY_ID']
       @secret_key = secret_key || ENV['AWS_SECRET_ACCESS_KEY']
       @security_token = security_token
       @region = region || ENV["AWS_DEFAULT_REGION"] || 'us-east-1'
-      @s3_endpoint = "https://#{bucket_name}.s3.#{@region}.amazonaws.com"
+      @s3_endpoint = ENV["MRB_AWS_S3_ENDPOINT"] || "https://s3.#{@region}.amazonaws.com"
+    end
+
+    def set_bucket(bucket_name)
+      @s3_endpoint = @s3_endpoint.sub('//', "//#{bucket_name}.")
       @s3_endpoint = HTTP::Parser.new.parse_url(@s3_endpoint)
       @http = SimpleHttp.new(@s3_endpoint.schema, @s3_endpoint.host, @s3_endpoint.port)
     end
 
     def download(path)
       headers = {
-        'Body' => '',
+        'Body' => ''
       }
       calculate_signature('GET', path, headers)
     end
 
     def upload(path, text)
       headers = {
-        'Body' => text,
+        'Body' => text
       }
       calculate_signature('PUT', path, headers)
     end
@@ -60,8 +64,7 @@ module AWS
       str_to_sign = "AWS4-HMAC-SHA256\n#{headers['x-amz-date']}\n#{scope.join '/'}"
       str_to_sign += "\n#{Digest::SHA256.hexdigest(canon_req)}"
 
-      signing_key = "AWS4#{@secret_key}"
-      signing_key = scope.inject(signing_key) do |k, v|
+      signing_key = scope.inject("AWS4#{@secret_key}") do |k, v|
         Digest::HMAC.digest(v, k, Digest::SHA256)
       end
 
